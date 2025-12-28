e06598e7e3c7c69fc8e84d3f5fcebf8e675fde3b
Kutay Eroğlu <kutay_eroglu@outlook.com>
2025-12-28 15:42:44 +0300

    Add slurm script for testing model implementation

---
+++ scripts/test_model.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --job-name=bio-alexnet
+#SBATCH --output=logs/bio-alexnet-%j.out
+#SBATCH --error=logs/bio-alexnet-%j.err
+
+#SBATCH --container-image ghcr.io\#kutayeroglu/biomim
+#SBATCH --container-mounts /stratch/dataset:/datasets
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+#SBATCH --time=01:00:00
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "--- Starting Job: $SLURM_JOB_ID ---"
+echo "Running on host: $(hostname)"
+echo "Allocated GPUs: $SLURM_GPUS_ON_NODE"
+echo ""
+
+cd "$HOME/projects/biomimetic-training"
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_GPU
+# Note: expandable_segments not supported on this platform
+# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+echo "--- Executing models script ---"
+
+python3 models.py
+
+echo "--- Job Finished Successfully ---"
+
